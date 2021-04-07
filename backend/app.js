8c778cdc8ee0ca4fa65b7bd8e739cd0a58a85048
Suyash <mandhanasuyash@yahoo.in>
2021-04-07 23:50:48 +0530

    implementing profile part

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ connect.then(() => {
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/userSignin");
+var profileRouter = require("./routes/userProfile")
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -30,7 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/signin", usersRouter);
-
+app.use("/profile",profileRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- backend/routes/userProfile.js
+++ backend/routes/userProfile.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 var express = require("express");
 var router = express.Router();
 const Profile = require("../schema/userProfileSchema.js");
@@ -11,4 +12,12 @@ router.get("/", function (req, res) {
 
 router.post("/", function (req, res) {
   let profile = new Profile();
+  profile.username = req.body.username;
+  profile.image = req.body.image;
+  profile.name = req.body.name;
+  profile.save().then(() => {
+    res.send("profile saved").catch((err) => {
+      res.send(err);
+    });
+  });
 });
