a94dbe414679c8e66a9ef9babe43d8226a829e0d
Suyash <mandhanasuyash@yahoo.in>
2021-04-07 23:47:23 +0530

    implementing profile part

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -2,10 +2,12 @@ import { Switch, Route } from "react-router-dom";
 import UserSignIn from "./SiginIn";
 import Home from "./Home";
 import Register from "./Register";
+import Profile from "./SellerProfile"
 
 const Navigate = () => {
   return (
     <Switch>
+      <Route exact path="/profile" component={Profile} />
       <Route exact path="/signin" component={UserSignIn} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/" component={Home} />
--- frontend/src/Components/SellerProfile.js
+++ frontend/src/Components/SellerProfile.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 const ProfileComponent = (props) => {
-  const [showDescriptionField, setShowDescriptionField] = useState(false);
+  const [showOrangeTypesField, setOrangeTypesField] = useState([]);
   const [sellerData, setSellerData] = useState(null);
-
+  console.log(showOrangeTypesField);
   useEffect(() => {
     axios
       .get("http://localhost:9000/profile", {
@@ -16,7 +17,47 @@ const ProfileComponent = (props) => {
       });
   }, []);
 
-  return <></>;
+  const addInputs = () => {
+    setOrangeTypesField([...showOrangeTypesField, 1]);
+  };
+
+  return (
+    <>
+      {sellerData ? (
+        <p>{sellerData}</p>
+      ) : (
+        <>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            {showOrangeTypesField.map(() => {
+              return (
+                <Form.Group>
+                  <Form.Control
+                    type="text"
+                    id="name"
+                    placeholder="name of the orange type"
+                  />
+                  <Form.Control
+                    type="text"
+                    id="rate"
+                    placeholder="rate per kg"
+                  />
+                </Form.Group>
+              );
+            })}
+          </Form>
+          <center>
+            <Button variant="primary" onClick={addInputs}>
+              Add Orange Type
+            </Button>
+          </center>
+        </>
+      )}
+    </>
+  );
 };
 
 const mapStateToProps = (state) => ({
